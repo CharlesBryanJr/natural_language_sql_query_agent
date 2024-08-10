7ad39c00f625f92415971086897c1ee5225c83f0
Charles <charlesbryan@Charless-MacBook-Pro.local>
2024-08-09 19:33:02 -0500

    all

--- server_python/controllers/agent_controller.py
+++ server_python/controllers/agent_controller.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import json
+import copy
 
 # Add the project root directory and server_python directory to the sys.path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
@@ -37,57 +38,83 @@ def ask_question():
         print('ask_question')
         data = request.get_json()
 
-        # Destructure the JSON data into individual variables
-        query = data.get('query')
-        database = data.get('database')
-        review_define_objective = data.get('review_define_objective')
-        review_before_generate = data.get('review_before_generate')
-        review_after_generate = data.get('review_after_generate')
-        review_after_reflect = data.get('review_after_reflect')
-        research_critique = data.get('research_critique')
-        Detailed = data.get('Detailed')
-        lastNode = data.get('lastNode')
-        nextNode = data.get('nextNode')
-        thread = data.get('thread')
-        draftRev = data.get('draftRev')
-        count = data.get('count')
-        writer_result = data.get('writer_result')
-        presigned_url = data.get('presigned_url')
+        fallback_data = {
+            "query": "write a SQL query calculates the total number of COVID-19 tests conducted in each state and then list these states in descending order based on the volume of tests, starting with the state that has conducted the most tests.",
+            "database": "covid-19",
+            "review_define_objective": False,
+            "review_before_generate": False,
+            "review_after_generate": False,
+            "review_after_reflect": False,
+            "research_critique": False,
+            "Detailed": False,
+            "lastNode": None,
+            "nextNode": None,
+            "thread": {"configurable": {"thread_id": "1"}},
+            "draftRev": 0,
+            "count": 0,
+            "writer_result": None,
+            "presigned_url": None
+        }
+
+        query = need_fallback_data(data, fallback_data, 'query')
+        database = need_fallback_data(data, fallback_data, 'database')
+        review_define_objective = need_fallback_data(data, fallback_data, 'review_define_objective')
+        review_before_generate = need_fallback_data(data, fallback_data, 'review_before_generate')
+        review_after_generate = need_fallback_data(data, fallback_data, 'review_after_generate')
+        review_after_reflect = need_fallback_data(data, fallback_data, 'review_after_reflect')
+        research_critique = need_fallback_data(data, fallback_data, 'research_critique')
+        Detailed = need_fallback_data(data, fallback_data, 'Detailed')
+        lastNode = need_fallback_data(data, fallback_data, 'lastNode')
+        nextNode = need_fallback_data(data,fallback_data, 'nextNode')
+        thread = need_fallback_data(data, fallback_data, 'thread')
+        draftRev = need_fallback_data(data, fallback_data, 'draftRev')
+        count = need_fallback_data(data, fallback_data, 'count')
+        writer_result = need_fallback_data(data, fallback_data, 'writer_result')
+        presigned_url = need_fallback_data(data, fallback_data, 'presigned_url')
+
 
         args = (query, database, review_define_objective, review_before_generate, review_after_generate, review_after_reflect, research_critique, Detailed, lastNode, nextNode, thread, draftRev, count, writer_result, presigned_url)
 
         # writer_result = writer(query)
         # writer_result, presigned_url = writer(query, database)
         # writer_result = "SELECT state, SUM(totaltestresults) AS total_tests_conducted FROM covid_testing_states_daily GROUP BY state ORDER BY total_tests_conducted DESC;"
-        (query, database, review_define_objective, review_before_generate, review_after_generate, review_after_reflect, research_critique, Detailed, lastNode, nextNode, thread, draftRev, count, writer_result, presigned_url) = writer(*args)
+        (result_query, result_database, result_review_define_objective, result_review_before_generate, 
+                result_review_after_generate, result_review_after_reflect, result_research_critique, 
+                result_Detailed, result_lastNode, result_nextNode, result_thread, result_draftRev, 
+                result_count, result_writer_result, result_presigned_url) = writer(*args)
 
         # presigned_url = execute_athena_query_function(writer_result, database)
         # presigned_url = "https://covid19-data-lake-results.s3.amazonaws.com/LLM/260cd42d-b2d5-42b3-8ef8-3129c369f61d.csv?response-expires=Thu%2C%2001%20Jan%201970%2000%3A06%3A00%20GMT&AWSAccessKeyId=AKIAQ3EGUGAZG4LXU3ES&Signature=f%2FU%2F13rGmVKQB6PI2pP6o34SpDYqvg%3D&Expires=1720111662"
 
-        result = {
-            "query": query,
-            "database": database,
-            "review_define_objective": review_define_objective,
-            "review_before_generate": review_before_generate,
-            "review_after_generate": review_after_generate,
-            "review_after_reflect": review_after_reflect,
-            "research_critique": research_critique,
-            "Detailed": Detailed,
-            "lastNode": lastNode,
-            "nextNode": nextNode,
-            "thread": thread,
-            "draftRev": draftRev,
-            "count": count,
-            "writer_result": writer_result,
-            "presigned_url": presigned_url
+        destructed = {
+            "query": result_query,
+            "database": result_database,
+            "review_define_objective": result_review_define_objective,
+            "review_before_generate": result_review_before_generate,
+            "review_after_generate": result_review_after_generate,
+            "review_after_reflect": result_review_after_reflect,
+            "research_critique": result_research_critique,
+            "Detailed": result_Detailed,
+            "lastNode": result_lastNode,
+            "nextNode": result_nextNode,
+            "thread": result_thread,
+            "draftRev": result_draftRev,
+            "count": result_count,
+            "writer_result": result_writer_result,
+            "presigned_url": result_presigned_url
         }
 
         print('-'*30)
-        return jsonify(result)
+        return jsonify(destructed)
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return jsonify({"error": str(e)}), 500
+
+
+def need_fallback_data(data, fallback_data, key):
+    value = data.get(key)
+    return value if value is not None else fallback_data.get(key)
 
 
 '''
--- server_python/routes/agent_routes.py
+++ server_python/routes/agent_routes.py
@@ -4,18 +4,11 @@ from flask import Blueprint, request, jsonify
 from flask_cors import CORS
 from server_python.controllers.agent_controller import ask_question
 
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(project_root)
+
 # Initialize the blueprint
 agent_bp = Blueprint('agent_bp', __name__)
 CORS(agent_bp)  # Enable CORS for this blueprint
 
-# Route definition
-@agent_bp.route('/ask', methods=['POST'])
-def ask():
-    if request.method == 'POST':
-        try:
-            data = request.json
-            response = ask_question()
-            return jsonify({"message": "Request received", "data": data}), 200  # Temporary response
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-    return jsonify({"message": "Method not allowed"}), 405
+agent_bp.route('/ask', methods=['POST'])(ask_question)
